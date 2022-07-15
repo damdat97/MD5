95b684017ebfd344ff65d6b8b50276c660722de5
damdat97 <dam.dat97@gmail.com>
2022-07-15 09:19:03 +0700

    TH - Article

--- BT/bai 3/angular-color-picker-app/src/app/app.module.ts
+++ BT/bai 3/angular-color-picker-app/src/app/app.module.ts
@@ -4,16 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
+import { ArticleComponent } from './article/article.component';
+import { LikeComponent } from './like/like.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColorPickerComponent
+    ColorPickerComponent,
+    ArticleComponent,
+    LikeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
